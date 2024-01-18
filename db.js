26276f4a35fc8c008f37a1adfab6fa0eb53069e1
adarshrawat <arawat@grepruby.io>
2024-01-18 17:38:14 +0530

    c

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 // const ConnectionURI = 'mongodb+srv://arawat1:<yotadota>@storefront.gsefmcd.mongodb.net/?retryWrites=true&w=majority'
-const ConnectionURI = process.env.DATABASE_URL.toString()
+const ConnectionURI = process.env.DATABASE_URI.toString()
 const client = new MongoClient(ConnectionURI);
 await client.connect();
 
